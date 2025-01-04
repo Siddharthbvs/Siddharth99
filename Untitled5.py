6d15b46fb7c005af178910f7749d49d1688bf769
Siddharthbvs <2311cs020061@mallareddyuniversity.ac.in>
2025-01-04 09:49:03 +0530

    Datatypes in python

---
+++ Untitled5.py
@@ -0,0 +1,22 @@
+
+
+integer_var = 42
+
+float_var = 3.14
+
+
+string_var = "Hello, Jupyter!"
+
+boolean_var = True
+
+print(f"Integer variable: {integer_var}, Type: {type(integer_var)}")
+print(f"Float variable: {float_var}, Type: {type(float_var)}")
+print(f"String variable: {string_var}, Type: {type(string_var)}")
+print(f"Boolean variable: {boolean_var}, Type: {type(boolean_var)}")
+
+
+# In[ ]:
+
+
+
+
